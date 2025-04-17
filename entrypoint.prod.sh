f4751450f712268509f070cd1f3c144bc2287014
Joshua Ruebeck <josh.ruebeck@gmail.com>
2025-04-17 00:01:40 -0500

    Change static to named volume to fix permissions

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
 python manage.py collectstatic --noinput
-echo "ran collectstatic"
 python manage.py migrate --noinput
 gunicorn --bind 0.0.0.0:8000 --workers 3 localmusic.wsgi:application
