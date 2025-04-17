1390fcbd57824a41b444abe657f9ecb4400d6598
Joshua Ruebeck <josh.ruebeck@gmail.com>
2025-04-17 10:19:13 -0500

    HTTPS fixes

--- update_config.sh
+++ update_config.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+# Set contents of .env as local variables
+. ./.env
+
 # Find & replace, creating new config file from template
 sed "s/TEMPLATE_HOST_NAME/$HOST_NAME/g" < nginx.template > nginx.conf
