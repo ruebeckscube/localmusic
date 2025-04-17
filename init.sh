eebc9886558eec2e1d743de935b0fc97b003b211
Joshua Ruebeck <josh.ruebeck@gmail.com>
2025-04-17 11:12:57 -0500

    Automate SSL certificate requests with init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,16 @@
 # Set contents of .env as local variables
 . ./.env
 
-sh ./update_config.sh
+# A minimal config for getting SSL certificates
+sh ./update_config.sh nginx_temp_cert.template
+
+# nginx must be running, and gotta build the whole thing anyway
+docker compose up --build
 
 # Set up initial SSL certificates
 docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d "$HOST_NAME"
+
+# Full config that can reference the SSL certificates now that they exist
+sh ./update_config.sh nginx.template
+
+docker compose restart
--- update_config.sh
+++ update_config.sh
@@ -4,4 +4,4 @@
 . ./.env
 
 # Find & replace, creating new config file from template
-sed "s/TEMPLATE_HOST_NAME/$HOST_NAME/g" < nginx.template > nginx.conf
+sed "s/TEMPLATE_HOST_NAME/$HOST_NAME/g" < "${1:-nginx.template}" > nginx.conf
