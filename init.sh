3341da96258ce590f004d626891b3d78510801ca
Joshua Ruebeck <josh.ruebeck@gmail.com>
2025-04-17 11:36:12 -0500

    Fix SSL automation

--- init.sh
+++ init.sh
@@ -4,15 +4,23 @@
 . ./.env
 
 # A minimal config for getting SSL certificates
+echo "Updating nginx to temp config to get SSL certificates"
 sh ./update_config.sh nginx_temp_cert.template
 
-# nginx must be running, and gotta build the whole thing anyway
-docker compose up --build
+# nginx must be running to respond to Certificate Authority requests, and gotta build the whole thing anyway
+docker compose up -d --build
 
-# Set up initial SSL certificates
+echo "Docker containers are building"
+while [ $(docker inspect -f '{{.State.Status}}' localmusic-proxy-1 2>/dev/null) != "running" ]
+do
+    sleep 1
+done
+
+
+echo "Setting up SSL certificates"
 docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d "$HOST_NAME"
 
-# Full config that can reference the SSL certificates now that they exist
+echo "Updating nginx config"
 sh ./update_config.sh nginx.template
 
 docker compose restart
