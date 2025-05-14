d7642b88b3c9608887611673925d0988c499ee38
Joshua Ruebeck <josh.ruebeck@gmail.com>
2025-05-14 14:36:27 -0500

    Switch some admin fields to select2 (autocomplete)

--- findshows/admin.py
+++ findshows/admin.py
@@ -5,15 +5,22 @@ from .models import Artist, ArtistLinkingInfo, CustomText, MusicBrainzArtist, Us
 class SetOrderInline(admin.TabularInline):
     model = SetOrder
     extra = 1 # how many rows to show
+    autocomplete_fields = ("artist",)
 
 class ConcertAdmin(admin.ModelAdmin):
     inlines = (SetOrderInline,)
+    autocomplete_fields = ('venue',)
+
+class VenueAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
 
 class UserProfileAdmin(admin.ModelAdmin):
-    readonly_fields=('favorite_musicbrainz_artists',)
+    readonly_fields = ('favorite_musicbrainz_artists',)
+    autocomplete_fields = ('followed_artists', 'managed_artists')
 
 class ArtistAdmin(admin.ModelAdmin):
     readonly_fields=('similar_musicbrainz_artists',)
+    search_fields=('name',)
 
 class MusicBrainzArtistAdmin(admin.ModelAdmin):
     readonly_fields=('mbid', 'name')
@@ -21,7 +28,7 @@ class MusicBrainzArtistAdmin(admin.ModelAdmin):
 admin.site.register(Concert, ConcertAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Artist, ArtistAdmin)
-admin.site.register(Venue)
+admin.site.register(Venue, VenueAdmin)
 admin.site.register(MusicBrainzArtist, MusicBrainzArtistAdmin)
 admin.site.register(ArtistLinkingInfo)
 admin.site.register(CustomText)
